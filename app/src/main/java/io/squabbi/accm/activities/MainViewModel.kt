1e8b2a83fbd36ff0b8504cc227ffbd7f3365985d
Jayden <squabbi@users.noreply.github.com>
2019-02-25 23:02:37 +1100

    Added Repository for ACC

--- app/src/main/java/io/squabbi/accm/activities/MainViewModel.kt
+++ app/src/main/java/io/squabbi/accm/activities/MainViewModel.kt
@@ -3,26 +3,34 @@ package io.squabbi.accm.activities
 import android.app.Application
 import android.os.Handler
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.topjohnwu.superuser.internal.UiThreadHandler.handler
 import io.squabbi.accm.models.AccInfo
+import io.squabbi.accm.repo.AccRepository
 import io.squabbi.accm.utils.AccUtils
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     //TODO: Create repository which does the live reading of acc info/status console output
+    private val repository: AccRepository
 
-    val mAccInfoLiveData: MutableLiveData<AccInfo> = MutableLiveData()
-
-    val accInfoRunnable = object: Runnable {
-        override fun run() {
-            mAccInfoLiveData.value = AccUtils.getBatteryInfo()
-            handler.postDelayed(this, 1000)
-        }
-    }
+    val mAccInfoLiveData: LiveData<AccInfo>
 
     init {
-        val handler: Handler = Handler()
-        handler.post(accInfoRunnable)
+        repository = AccRepository()
+        mAccInfoLiveData = repository.mAccInfoLiveData
     }
+
+//    val accInfoRunnable = object: Runnable {
+//        override fun run() {
+//            mAccInfoLiveData.value = AccUtils.getAccInfo()
+//            handler.postDelayed(this, 1000)
+//        }
+//    }
+//
+//    init {
+//        val handler: Handler = Handler()
+//        handler.post(accInfoRunnable)
+//    }
 }
