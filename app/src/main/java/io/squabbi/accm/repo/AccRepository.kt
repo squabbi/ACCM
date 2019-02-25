a9dcd1fcd029fda296a87bd65e1e293907dd4677
Jayden <squabbi@users.noreply.github.com>
2019-02-25 19:47:41 +1100

    Create AccRepository.kt

---
+++ app/src/main/java/io/squabbi/accm/repo/AccRepository.kt
@@ -0,0 +1,27 @@
+package io.squabbi.accm.repo
+
+import android.os.Handler
+import androidx.lifecycle.MutableLiveData
+import com.topjohnwu.superuser.internal.UiThreadHandler
+import io.squabbi.accm.models.AccInfo
+import io.squabbi.accm.utils.AccUtils
+
+/**
+ * Repository class for grabbing acc commands.
+ */
+class AccRepository {
+
+    val mAccInfoLiveData: MutableLiveData<AccInfo> = MutableLiveData()
+
+    val accInfoRunnable = object: Runnable {
+        override fun run() {
+            mAccInfoLiveData.value = AccUtils.getBatteryInfo()
+            UiThreadHandler.handler.postDelayed(this, 1000)
+        }
+    }
+
+    init {
+        val handler: Handler = Handler()
+        handler.post(accInfoRunnable)
+    }
+}
